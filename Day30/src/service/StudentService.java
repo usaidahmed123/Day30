ecee7c3edf01707aa927c85f8d0bad42d41c5b7e
jawad <jawadusaid11@gmail.com>
2022-03-23 14:05:21 +0530

    JPA

---
+++ Day30/src/service/StudentService.java
@@ -0,0 +1,11 @@
+package service;
+
+
+import entities.Student;
+public interface StudentService {
+public abstract void addStudent(Student student);
+public abstract void updateStudent(Student student);
+public abstract void removeStudent(Student student);
+
+public abstract Student findStudentById(int id);
+}
